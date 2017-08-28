470ce769cf54926c3e3c26338f47c9f557cbd0f8
Singh400 <Singh400>
2017-08-28 18:23:51 +0100

    Spiking out System.Buffers.ArrayPool
    Following on from https://github.com/mgravell/protobuf-net/issues/301#issuecomment-325216098 I found System.Buffers.
    
    This is version matches WithCachedPool version. Same 90% reducation in LOH allocations.
    
    N.B.: .Fllush() throws NotImplementedException. Not sure how to do with with ArrayPool just yet.

--- src/protobuf-net/BufferPool.cs
+++ src/protobuf-net/BufferPool.cs
@@ -1,51 +1,29 @@
-﻿
-using System.Threading;
+﻿using System;
+using System.Buffers;
+
 namespace ProtoBuf
 {
     internal sealed class BufferPool
     {
+        private const int PoolSize = 20;
+        private const int BufferLength = 1024;
+        private static readonly ArrayPool<byte> _arrayPool;
+
+        static BufferPool()
+        {
+            _arrayPool = ArrayPool<byte>.Create(maxArrayLength: BufferLength * BufferLength * BufferLength, maxArraysPerBucket: PoolSize);
+        }
+
         internal static void Flush()
         {
-#if PLAT_NO_INTERLOCKED
-            lock(pool)
-            {
-                for (int i = 0; i < pool.Length; i++) pool[i] = null;
-            }
-#else
-            for (int i = 0; i < pool.Length; i++)
-            {
-                Interlocked.Exchange(ref pool[i], null); // and drop the old value on the floor
-            }
-#endif
+            throw new NotImplementedException(); // Not sure how to flush the existing _arrayPool - overwrite reference?
         }
-        private BufferPool() { }
-        const int PoolSize = 20;
-        internal const int BufferLength = 1024;
-        private static readonly object[] pool = new object[PoolSize];
 
         internal static byte[] GetBuffer()
         {
-            object tmp;
-            #if PLAT_NO_INTERLOCKED
-            lock(pool)
-            {
-                for (int i = 0; i < pool.Length; i++)
-                {
-                    if((tmp = pool[i]) != null)
-                    {
-                        pool[i] = null;
-                        return (byte[])tmp;
-                    }
-                }
-            }
-#else
-            for (int i = 0; i < pool.Length; i++)
-            {
-                if ((tmp = Interlocked.Exchange(ref pool[i], null)) != null) return (byte[])tmp;
-            }
-#endif
-            return new byte[BufferLength];
+            return _arrayPool.Rent(BufferLength);
         }
+
         internal static void ResizeAndFlushLeft(ref byte[] buffer, int toFitAtLeastBytes, int copyFromIndex, int copyBytes)
         {
             Helpers.DebugAssert(buffer != null);
@@ -55,49 +33,31 @@ namespace ProtoBuf
 
             // try doubling, else match
             int newLength = buffer.Length * 2;
-            if (newLength < toFitAtLeastBytes) newLength = toFitAtLeastBytes;
 
-            byte[] newBuffer = new byte[newLength];
+            if (newLength < toFitAtLeastBytes)
+            {
+                newLength = toFitAtLeastBytes;
+            }
+
+            if (copyBytes == 0)
+            {
+                ReleaseBufferToPool(ref buffer); // No need to copy, we can release immediately
+            }
+
+            byte[] newBuffer = _arrayPool.Rent(newLength);
+
             if (copyBytes > 0)
             {
                 Helpers.BlockCopy(buffer, copyFromIndex, newBuffer, 0, copyBytes);
+                ReleaseBufferToPool(ref buffer);
             }
-            if (buffer.Length == BufferPool.BufferLength)
-            {
-                BufferPool.ReleaseBufferToPool(ref buffer);
-            }
+
             buffer = newBuffer;
         }
+
         internal static void ReleaseBufferToPool(ref byte[] buffer)
         {
-            if (buffer == null) return;
-            if (buffer.Length == BufferLength)
-            {
-#if PLAT_NO_INTERLOCKED
-                lock (pool)
-                {
-                    for (int i = 0; i < pool.Length; i++)
-                    {
-                        if(pool[i] == null)
-                        {
-                            pool[i] = buffer;
-                            break;
-                        }
-                    }
-                }
-#else
-                for (int i = 0; i < pool.Length; i++)
-                {
-                    if (Interlocked.CompareExchange(ref pool[i], buffer, null) == null)
-                    {
-                        break; // found a null; swapped it in
-                    }
-                }
-#endif
-            }
-            // if no space, just drop it on the floor
-            buffer = null;
+            _arrayPool.Return(array: buffer, clearArray: true);
         }
-
     }
 }
